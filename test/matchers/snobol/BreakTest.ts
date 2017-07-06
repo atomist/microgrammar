b93fb36e7621f105504af7b56b787d6f0b1a161c
Rod Johnson <rod@atomist.com>
2017-07-06 12:21:06 +1000

    Test polishing

--- test/matchers/snobol/BreakTest.ts
+++ test/matchers/snobol/BreakTest.ts
@@ -1,4 +1,3 @@
-import { expect } from "chai";
 import { Term } from "../../../src/Matchers";
 import { Concat } from "../../../src/matchers/Concat";
 import { isPatternMatch, PatternMatch } from "../../../src/PatternMatch";
@@ -8,6 +7,9 @@ import { Break } from "../../../src/matchers/snobol/Break";
 
 import { inputStateFromString } from "../../../src/internal/InputStateFactory";
 import { Span } from "../../../src/matchers/snobol/Span";
+import { Alt } from "../../../src/Ops";
+
+import * as assert from "power-assert";
 
 describe("Break", () => {
 
@@ -15,16 +17,16 @@ describe("Break", () => {
         const b = new Break("14");
         const is = inputStateFromString("");
         const m = b.matchPrefix(is, {});
-        expect(isPatternMatch(m)).to.equal(true);
-        expect((m as PatternMatch).$matched).to.equal("");
+        assert(isPatternMatch(m));
+        assert((m as PatternMatch).$matched === "");
     });
 
     it("break matches another matcher", () => {
         const b = new Break(new Regex(/^[a-z]/));
         const is = inputStateFromString("HEY YOU banana");
         const m = b.matchPrefix(is, {});
-        expect(isPatternMatch(m)).to.equal(true);
-        expect((m as PatternMatch).$matched).to.equal("HEY YOU ");
+        assert(isPatternMatch(m));
+        assert((m as PatternMatch).$matched === "HEY YOU ");
     });
 
     it("break matches a complicated matcher", () => {
@@ -33,33 +35,46 @@ describe("Break", () => {
                 { consumeWhiteSpaceBetweenTokens: false }));
         const is = inputStateFromString("HEY YOU ${thing} and more stuff");
         const m = b.matchPrefix(is, {});
-        expect(isPatternMatch(m)).to.equal(true);
-        expect((m as PatternMatch).$matched).to.equal("HEY YOU ");
+        assert(isPatternMatch(m));
+        assert((m as PatternMatch).$matched === "HEY YOU ");
     });
 
     it("break matches", () => {
         const b = new Break("14");
         const is = inputStateFromString("friday 14");
         const m = b.matchPrefix(is, {});
-        expect(isPatternMatch(m)).to.equal(true);
-        expect((m as PatternMatch).$matched).to.equal("friday ");
+        assert(isPatternMatch(m));
+        assert((m as PatternMatch).$matched === "friday ");
+    });
+
+    it("break to Alt", () => {
+        const b = new Break(new Alt("14", "44"));
+        const is = inputStateFromString("friday 14");
+        const m = b.matchPrefix(is, {});
+        assert(isPatternMatch(m));
+        assert((m as PatternMatch).$matched === "friday ");
+
+        const is2 = inputStateFromString("friday 44");
+        const m2 = b.matchPrefix(is2, {});
+        assert(isPatternMatch(m2));
+        assert((m2 as PatternMatch).$matched === "friday ");
     });
 
     it("break matches and consumes", () => {
         const b = new Break("14", true);
         const is = inputStateFromString("friday 14");
         const m = b.matchPrefix(is, {}) as any;
-        expect(isPatternMatch(m)).to.equal(true);
-        expect(m.$matched).to.equal("friday 14");
-        expect(m.$value).to.equal("14");
+        assert(isPatternMatch(m));
+        assert(m.$matched === "friday 14");
+        assert(m.$value === "14");
     });
 
     it("break matches nothing as it comes immediately", () => {
         const b = new Break("friday");
         const is = inputStateFromString("friday 14");
         const m = b.matchPrefix(is, {});
-        expect(isPatternMatch(m)).to.equal(true);
-        expect((m as PatternMatch).$matched).to.equal("");
+        assert(isPatternMatch(m));
+        assert((m as PatternMatch).$matched === "");
     });
 
     it("break matches all in concat", () => {
@@ -70,8 +85,8 @@ describe("Break", () => {
         });
         const is = inputStateFromString("friday 14");
         const m = c.matchPrefix(is, {});
-        expect(isPatternMatch(m)).to.equal(true);
-        expect((m as PatternMatch).$matched).to.equal("friday 14");
+        assert(isPatternMatch(m));
+        assert((m as PatternMatch).$matched === "friday 14");
     });
 
 });
