af378d98b53580e5150fa4ddf5b97043945fa1df
Rod Johnson <rod@atomist.com>
2017-07-15 17:50:05 +1000

    Move from chai to power assert

--- src/internal/SpecGrammar.ts
+++ src/internal/SpecGrammar.ts
@@ -1,29 +1,28 @@
 import { Concat } from "../matchers/Concat";
 import { RestOfInput } from "../matchers/skip/Skip";
-import { Literal, Regex } from "../Primitives";
+import { Regex } from "../Primitives";
 import { Rep } from "../Rep";
 import { Break } from "./Break";
 
 const elementReference = new Concat({
     $id: "component",
-    _start: new Literal("${"),
+    _start: "${",
     elementName: new Regex(/[a-zA-Z0-9_]+/),
-    _end: new Literal("}"),
+    _end: "}",
 });
 
 export const specGrammar = new Concat({
     $id: "spec",
-    these: new Rep(
-        new Concat({
-            $id: "literal, then component",
-            literal: new Break(elementReference),
-            element: elementReference,
-        })),
+    these: new Rep({
+        $id: "literal, then component",
+        literal: new Break(elementReference),
+        element: elementReference,
+    }),
     trailing: RestOfInput, //  matchEverything
 
 });
 
 export interface MicrogrammarSpec {
-    these: Array<{literal: string, element: { elementName: string }}>;
+    these: Array<{ literal: string, element: { elementName: string } }>;
     trailing: string;
 }
