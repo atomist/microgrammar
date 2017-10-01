2ea8a84a17291d98512ca54ebb63898ae7b16976
David Dooling <david@atomist.com>
2017-10-01 11:19:23 -0500

    Publish master and PR builds to team NPM registry
    Modify Travis build script to publish successful master and PR builds
    to the Atomist NPM registry on Artifactory.  Refactor script for more
    reuse and easier to follow flow.  Move all publishing into build
    script.

--- scripts/travis-build.bash
+++ scripts/travis-build.bash
@@ -4,7 +4,7 @@
 set -o pipefail
 
 declare Pkg=travis-build-node
-declare Version=0.3.0
+declare Version=0.4.0
 
 function msg() {
     echo "$Pkg: $*"
@@ -12,6 +12,226 @@ function msg() {
 
 function err() {
     msg "$*" 1>&2
+}
+
+# use environment variables to create .npmrc
+function create-npmrc () {
+    local npmrc=$HOME/.npmrc
+    rm -f "$npmrc"
+    if ! ( umask 077 && echo -e 'always-auth = true\nemail = travis-ci@atomist.com' > "$npmrc" )
+    then
+        err "failed to create $npmrc"
+        return 1
+    fi
+
+    if [[ $NPM_TOKEN ]]; then
+        msg "adding NPM registry token to $npmrc"
+        if ! echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> "$npmrc"; then
+            err "failed to add NPM token to $npmrc"
+            return 1
+        fi
+    fi
+    if [[ $NPM_REGISTRY && $ARTIFACTORY_TOKEN ]]; then
+        local registry
+        registry=$(echo "$NPM_REGISTRY" | sed -e 's/https\{0,1\}://' -e 's/[^\/]\{1,\}$//')
+        if [[ $? -ne 0 || ! $registry ]]; then
+            err "failed to parse registry base: $NPM_REGISTRY"
+            return 1
+        fi
+        msg "adding Artifactory registry $registry token to $npmrc"
+        if ! echo "$registry:_authToken=$ARTIFACTORY_TOKEN" >> "$npmrc"
+        then
+            err "failed to add Artifactory token to $npmrc"
+            return 1
+        fi
+    fi
+}
+
+# upsert a value in package.json
+function edit-package-json () {
+    local jpath=$1
+    if [[ ! $jpath ]]; then
+        err "edit-package-json: missing required argument: JSON_PATH"
+        return 10
+    fi
+    shift
+    local value=$1
+    if [[ ! $value ]]; then
+        err "set-version: missing required argument: VALUE"
+        return 10
+    fi
+    shift
+
+    local pkg_tmp pkg_json=package.json
+    pkg_tmp=$(mktemp "$pkg_json.XXXXXX")
+    if [[ $? -ne 0 || ! $pkg_tmp ]]; then
+        err "failed to create temporary file"
+        return 1
+    fi
+    trap "rm -f $pkg_tmp" RETURN
+
+    if ! cat "$pkg_json" > "$pkg_tmp"; then
+        err "failed to copy package.json to $pkg_tmp"
+        return 1
+    fi
+
+    if ! jq -e "$jpath=\"$value\"" "$pkg_tmp" > "$pkg_json"; then
+        err "failed to update $jpath in $pkg_json: $value"
+        return 1
+    fi
+}
+
+# git tag and push
+function git-tag () {
+    local tag=$1
+    if [[ ! $tag ]]; then
+        err "git-tag: missing required argument: TAG"
+        return 10
+    fi
+
+    if ! git config --global user.email "travis-ci@atomist.com"; then
+        err "failed to set git user email"
+        return 1
+    fi
+    if ! git config --global user.name "Travis CI"; then
+        err "failed to set git user name"
+        return 1
+    fi
+    if ! git tag "$tag" -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"; then
+        err "failed to create git tag: $tag"
+        return 1
+    fi
+    local remote=origin
+    if [[ $GITHUB_TOKEN ]]; then
+        remote=https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG
+    fi
+    if ! git push --quiet "$remote" "$tag" > /dev/null 2>&1; then
+        err "failed to push git tag: $tag"
+        return 1
+    fi
+}
+
+# npm publish
+function npm-publish () {
+    msg "packaging module"
+    if ! cp -r build/src/* .; then
+        err "packaging module failed"
+        return 1
+    fi
+
+    # npm honors this
+    rm -f .gitignore
+
+    if ! npm publish "$@"; then
+        err "failed to publish node module"
+        cat npm-debug.log
+        return 1
+    fi
+
+    if ! git checkout -- .gitignore; then
+        err "removed .gitignore and was unable to check out original"
+        return 1
+    fi
+
+    local pub_file pub_base
+    for pub_file in build/src/*; do
+        pub_base=${$pub_file##*/}
+        rm -rf "$pub_base"
+    done
+}
+
+# publish a public version of master to non-standard registry
+function npm-publish-master () {
+    if [[ ! $NPM_REGISTRY ]]; then
+        msg "no team registry set"
+        return 0
+    fi
+
+    local pkg_version
+    pkg_version=$(jq -e --raw-output .version package.json)
+    if [[ $? -ne 0 || ! $pkg_version ]]; then
+        err "failed to parse version from package.json"
+        return 1
+    fi
+    local timestamp
+    timestamp=$(date -u +%Y%m%d%H%M%S)
+    if [[ $? -ne 0 || ! $timestamp ]]; then
+        err "failed to generate timestamp"
+        return 1
+    fi
+    local project_version=$pkg_version-$timestamp
+    if ! npm version "$project_version"; then
+        err "failed to set master build version: $project_version"
+        return 1
+    fi
+
+    if ! npm-publish --registry "$NPM_REGISTRY" --access public; then
+        err "failed to publish to Artifactory NPM registry"
+        return 1
+    fi
+
+    if ! git-tag "$project_version+travis$TRAVIS_BUILD_NUMBER"; then
+        return 1
+    fi
+}
+
+# publish a restricted version of a PR build
+function npm-publish-pr () {
+    if [[ ! $NPM_REGISTRY ]]; then
+        msg "no team registry set"
+        return 0
+    fi
+
+    if [[ $TRAVIS_PULL_REQUEST_BRANCH == master ]]; then
+        msg "will not publish PR from $TRAVIS_PULL_REQUEST_BRANCH"
+        return 0
+    fi
+    msg "attempting to publish PR build on branch $TRAVIS_PULL_REQUEST_BRANCH"
+
+    local name pkg_json=package.json
+    name=$(jq -e --raw-output .name "$pkg_json")
+    if [[ $? -ne 0 || ! $name ]]; then
+        err "failed to parse name in $pkg_json: $name"
+        return 1
+    fi
+    local branch_module_name="${name}_$TRAVIS_PULL_REQUEST_BRANCH"
+    if ! edit-package-json .name "$branch_module_name"; then
+        return 1
+    fi
+
+    # is there already one of these published?
+    local published_version
+    published_version=$(npm show "$branch_module_name" version)
+    if [[ $? -ne 0 || ! $published_version ]] ; then
+        msg "looks like this is the first time we've published this branch, cool"
+    else
+        if ! npm version "$published_version"; then
+            return 1
+        fi
+        if ! npm version --no-git-tag-version -f patch; then
+            err "failed to increment version in $pkg_json"
+            return 1
+        fi
+    fi
+    local pkg_version
+    pkg_version=$(jq -e --raw-output .version "$pkg_json")
+    if [[ $? -ne 0 || ! $pkg_version ]]; then
+        err "failed to parse version from $pkg_json"
+        return 1
+    fi
+
+    if ! npm-publish --registry "$NPM_REGISTRY" --access public; then
+        err "npm publish of PR build failed"
+        return 1
+    fi
+    msg "published PR build to npm as $branch_module_name version $pkg_version"
+
+    if ! git checkout -- "$pkg_json"; then
+        err "updated $pkg_json and could not check out the original"
+    fi
+    if ! git-tag "${branch_module_name}-${pkg_version}"; then
+        return 1
+    fi
 }
 
 # usage: main "$@"
@@ -34,137 +254,27 @@ function main () {
         return 1
     fi
 
-
-    local git_tag
-    # Publishing the branch privately to npm lets us test downstream projects
-    if [[ $TRAVIS_PULL_REQUEST != false && $TRAVIS_PULL_REQUEST_BRANCH != master ]] ; then
-        msg "I am a PR build! I have a branch! I will attempt to publish to NPM!"
-        if [[ $NPM_TOKEN ]] ; then
-            local current_module_name
-            current_module_name=$(jq --raw-output .name package.json)
-            if [[ $? -ne 0 || ! $current_module_name ]]; then
-                err "failed to parse name in package.json: $current_module_name"
-                return 1
-            fi
-            local branch_module_name
-            branch_module_name="${current_module_name}_$TRAVIS_PULL_REQUEST_BRANCH"
-
-            # update the package.json
-            local temp_package_json
-            temp_package_json=$(mktemp)
-            if [[ $? -ne 0 ]]; then
-                err "failed to get a temp file"
-                return 1
-            fi
-
-            if ! mv package.json "$temp_package_json"; then
-                err "failed to rename package.json to $temp_package_json"
-                return 1
-            fi
-            if ! jq -e ".name=\"$branch_module_name\"" "$temp_package_json" > package.json; then
-                err "failed to update name in package.json"
-                return 1
-            fi
-            trap "rm -f $temp_package_json" RETURN
-
-            # create this file so we can use npm show on private modules
-            msg "creating local .npmrc using NPM token from environment"
-            if ! ( umask 077 && echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > "$HOME/.npmrc" ); then
-                err "failed to create $HOME/.npmrc"
-                return 1
-            fi
-
-            # is there already one of these published ?
-            local last_existing_version
-            last_existing_version=$(npm show "$branch_module_name" version)
-            if [[ $? -ne 0 || ! $last_existing_version ]] ; then
-                # probably not
-                msg "Looks like this is the first time we've published this branch, cool"
-            else
-                # increment the version. First set us to the current, then bump.
-                if ! mv package.json "$temp_package_json"; then
-                    err "failed to rename package.json to $temp_package_json, the second time"
-                    return 1
-                fi
-                if ! jq -e ".version=\"$last_existing_version\"" "$temp_package_json" > package.json; then
-                    err "failed to update version in package.json"
-                    return 1
-                fi
-
-                if ! npm version --no-git-tag-version -f patch; then
-                    err "failed to increment version in package.json"
-                    return 1
-                fi
-            fi
-
-            # what version did we pick?
-            local pkg_version
-            pkg_version=$(jq --raw-output .version package.json)
-            if [[ $? -ne 0 || ! $pkg_version ]]; then
-                err "failed to parse version from package.json"
-                return 1
-            fi
-
-            if ! bash scripts/npm-publish.bash --access restricted; then
-                err "fail at npm publishing"
-                return 1
-            fi
-
-            msg "Published to npm as ${branch_module_name} version ${pkg_version}"
-            git_tag="${branch_module_name}-${pkg_version}"
-
-            if ! git checkout -- package.json; then
-                msg "WARNING: I changed package.json and couldn't check out the original"
-            fi
-        else
-            msg "No NPM_TOKEN, couldn't publish"
-        fi
+    if ! create-npmrc; then
+        err "failed to create .npmrc"
+        return 1
     fi
 
-    if [[ $TRAVIS_PULL_REQUEST == false ]] ; then
-        if [[ $TRAVIS_BRANCH == master || $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+(-(m|rc)\.[0-9]+)?$ ]]; then
-            local project_version
-            if [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+(-(m|rc)\.[0-9]+)?$ ]]; then
-                project_version=$TRAVIS_TAG
-            else
-                local pkg_version
-                pkg_version=$(jq --raw-output .version package.json)
-                if [[ $? -ne 0 || ! $pkg_version ]]; then
-                    err "failed to parse version from package.json"
-                    return 1
-                fi
-                local timestamp
-                timestamp=$(date -u +%Y%m%d%H%M%S)
-                if [[ $? -ne 0 || ! $timestamp ]]; then
-                    err "failed to generate timestamp"
-                    return 1
-                fi
-                project_version=$pkg_version-$timestamp
-            fi
-            git_tag=$project_version+travis$TRAVIS_BUILD_NUMBER
-        fi
-    fi
-
-
-    if [[ $git_tag ]] ; then
-        if ! git config --global user.email "travis-ci@atomist.com"; then
-            err "failed to set git user email"
+    if [[ $TRAVIS_PULL_REQUEST != false ]] ; then
+        if ! npm-publish-pr; then
+            err "failed to publish PR build"
             return 1
         fi
-        if ! git config --global user.name "Travis CI"; then
-            err "failed to set git user name"
+    elif [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+(-(m|rc)\.[0-9]+)?$ ]]; then
+        if ! npm-publish --access public; then
+            err "failed to publish tag build: $TRAVIS_TAG"
             return 1
         fi
-        if ! git tag "$git_tag" -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"; then
-            err "failed to create git tag: $git_tag"
+        if ! git-tag "$TRAVIS_TAG+travis$TRAVIS_BUILD_NUMBER"; then
             return 1
         fi
-        local remote=origin
-        if [[ $GITHUB_TOKEN ]]; then
-            remote=https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG
-        fi
-        if ! git push --quiet --tags "$remote" > /dev/null 2>&1; then
-            err "failed to push git tags"
+    elif [[ $TRAVIS_BRANCH == master ]]; then
+        if ! npm-publish-master; then
+            err "failed to publish master build"
             return 1
         fi
     fi
