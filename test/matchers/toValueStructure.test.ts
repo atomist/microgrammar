43a4eecadfb5eb562db9ceac22db9ed7040601dd
Jessica Kerr <jessitron@atomist.com>
2019-02-06 22:04:15 -0600

    Optional just works

--- test/matchers/toValueStructure.test.ts
+++ test/matchers/toValueStructure.test.ts
@@ -1,5 +1,5 @@
 import * as assert from "assert";
-import { firstOf } from "../../lib";
+import { firstOf, optional } from "../../lib";
 import { inputStateFromString } from "../../lib/internal/InputStateFactory";
 import { toValueStructure } from "../../lib/MatchReport";
 import { Literal } from "../../lib/Primitives";
@@ -22,4 +22,24 @@ describe("Converting a match report to a value structure", () => {
 
         assert.strictEqual(valueStructure, "lizards");
     });
+
+    it("gets undefined from an optional with unsuccessful inner",
+        () => {
+            const mg = optional("foo");
+            const inputString = "anything that does not start with foo";
+            const report = mg.matchPrefixReport(inputStateFromString(inputString), {}, {});
+            const vs = toValueStructure(report);
+
+            assert.strictEqual(vs, undefined);
+        });
+
+    it("gets the inner value from an optional with successful inner",
+        () => {
+            const mg = optional("foo");
+            const inputString = "foo stuff";
+            const report = mg.matchPrefixReport(inputStateFromString(inputString), {}, {});
+            const vs = toValueStructure(report);
+
+            assert.strictEqual(vs, "foo");
+        });
 });
